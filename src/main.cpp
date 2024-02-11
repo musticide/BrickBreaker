f12f969823109776241ea488a807297c399057e3
musticide <mustafa25.tm@gmail.com>
2024-02-11 09:06:09 +0530

    Added movement and collision. Fix needed for collision

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,66 @@
+#include "SFML/Graphics/RectangleShape.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Window/Keyboard.hpp"
 #include <SFML/Graphics.hpp>
 
-int
-main (int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  sf::RenderWindow window (sf::VideoMode (1280, 720), "SFML works!",
-                           sf::Style::None | sf::Style::Close);
-  sf::CircleShape shape (100.f);
-  shape.setFillColor (sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!", sf::Style::None | sf::Style::Close);
 
-  while (window.isOpen ())
-    {
-      sf::Event event;
-      while (window.pollEvent (event))
-        {
-          if (event.type == sf::Event::Closed)
-            window.close ();
+    sf::RectangleShape player(*new sf::Vector2f(100.f, 20.f));
+    player.setOrigin(50.f, 20.f);
+
+    sf::Vector2f playerPos(200.f, 300.f);
+
+    while (window.isOpen()) {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed)
+                window.close();
         }
 
-      window.clear ();
-      if (window.hasFocus ())
-        {
-          shape.setFillColor (sf::Color::Yellow);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            playerPos.x -= .5f;
         }
-      else
-        {
-          shape.setFillColor (sf::Color::Green);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            playerPos.x += .5f;
         }
-      window.draw (shape);
-      window.display ();
+
+        // moving the shape
+
+        playerPos.y += .5f;
+
+        // world bounds collision
+        // Right
+        if (player.getPosition().x > window.getSize().x - (player.getSize().x / 2)) {
+            playerPos.x = window.getSize().x - (player.getSize().x / 2);
+        }
+        // Left
+        if (player.getPosition().x < (player.getSize().x / 2)) {
+            playerPos.x = (player.getSize().x / 2);
+        }
+        // Top
+        if (player.getPosition().y < (player.getSize().y / 2)) {
+            playerPos.y = (player.getSize().y / 2);
+        }
+        // Bottom
+        if (player.getPosition().y > window.getSize().y - (player.getSize().y / 2)) {
+            playerPos.y = window.getSize().y - (player.getSize().y / 2);
+        }
+
+        player.setPosition(playerPos);
+
+        // draw shape
+        window.clear();
+        if (window.hasFocus()) {
+            player.setFillColor(sf::Color(65, 105, 255, 255));
+        } else {
+            player.setFillColor(sf::Color(65, 105, 255, 100));
+        }
+        window.draw(player);
+        window.display();
     }
 
-  return 0;
+    return 0;
 }
